f2d3330682d55f745a1356ea4fb08c3a6d75f7f2
Серега Каланджий <kjserg@i.ua>
2019-12-01 12:22:53 +0200

    finish index page

--- wp-content/themes/SiberianLawCompany/index.php
+++ wp-content/themes/SiberianLawCompany/index.php
@@ -269,6 +269,205 @@ $email = get_field('admin_email', $set);
                 </section>
 
             <?php endif; ?>
+        <?php elseif ( get_row_layout() == 'screen_8' ): ?>
+            <?php if (!get_sub_field('screen_off')) : ?>
+
+                <section class="section facts-section" id="<?php e('screen_id'); ?>">
+                    <div class="container">
+                        <div class="facts">
+                            <h3 class="section__title facts__title">
+                                <?php e('title'); ?>
+                            </h3>
+                            <?php if (have_rows('list')): $counter = 0; ?>
+                                <div class="facts-rows">
+                                    <ul class="facts-row">
+
+                                        <?php $i = 1; while (have_rows('list')) : the_row(); if($i<=3): ?>
+
+                                            <li class="facts-row__item">
+
+                                                <?php $text_1 = g('text_1'); ?>
+
+                                                <div class="facts-row__number <?php if ($text_1) echo 'facts-row__number--subrow'; ?> ">
+                                                    <?php if ($text_1): ?>
+                                                        <span>
+                                                            <?php e('number'); ?>
+                                                        </span>
+                                                        <span>
+                                                            <?php echo $text_1; ?>
+                                                        </span>
+                                                    <?php else: ?>
+                                                        <?php e('number'); ?>
+                                                    <?php endif; ?>
+                                                </div>
+                                                <div class="facts-row__text">
+                                                    <p>
+                                                        <?php e('text'); ?>
+                                                    </p>
+                                                </div>
+                                            </li>
+
+                                        <?php endif; $i++; $counter = $i; endwhile; ?>
+                                    </ul>
+                                    <?php if($counter > 3): ?>
+                                        <ul class="facts-row">
+                                            <?php $i = 1; while (have_rows('list')) : the_row(); if($i>3): ?>
+
+                                                <li class="facts-row__item">
+
+                                                    <?php $text_1 = g('text_1'); ?>
+
+                                                    <div class="facts-row__number <?php if ($text_1) echo 'facts-row__number--subrow'; ?> ">
+                                                        <?php if ($text_1): ?>
+                                                            <span>
+                                                                                    <?php e('number'); ?>
+                                                                                </span>
+                                                            <span>
+                                                                                    <?php echo $text_1; ?>
+                                                                                </span>
+                                                        <?php else: ?>
+                                                            <?php e('number'); ?>
+                                                        <?php endif; ?>
+                                                    </div>
+                                                    <div class="facts-row__text">
+                                                        <p>
+                                                            <?php e('text'); ?>
+                                                        </p>
+                                                    </div>
+                                                </li>
+
+                                            <?php endif; $i++; $counter = $i; endwhile; ?>
+                                        </ul>
+                                    <?php endif; ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="facts-additionally" style="background-image: linear-gradient(-8deg, #dbc6a2 0%, #f9e6c2 99%, #f9e6c2 100%); background-color: #dfb568;">
+                        <div class="container">
+                            <div class="facts-additionally-container">
+                                <div class="facts-additionally__image">
+                                    <img src="<?php echo $assets; ?>img/warning.png" alt="">
+                                </div>
+                                <div class="facts-additionally__text">
+                                    <p>
+                                        <?php e('text'); ?>
+                                        <?php if (g('link')): ?>
+                                            <a href="<?php e('link'); ?>" class="more">. . .</a>
+                                        <?php endif; ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            <?php endif; ?>
+        <?php elseif ( get_row_layout() == 'screen_9' ): ?>
+            <?php if (!get_sub_field('screen_off')) : ?>
+
+                <section class="section price-section" id="<?php e('screen_id'); ?>">
+                    <div class="container">
+                        <div class="price">
+                            <h3 class="section__title price__title">
+                                <?php e('title'); ?>
+                            </h3>
+
+                            <?php if (have_rows('table')): ?>
+
+                                <div class="price-container">
+                                    <div class="price-row">
+                                        <?php while (have_rows('table')) : the_row(); ?>
+                                            <div class="price-row__item">
+                                                <div class="price-row__name">
+                                                    <?php e('name'); ?>
+                                                </div>
+                                                <div class="price-row__dot"></div>
+                                                <div class="price-row__val">
+                                                    <?php e('val'); ?>
+                                                </div>
+                                            </div>
+                                        <?php endwhile; ?>
+                                    </div>
+                                </div>
+
+                            <?php endif; ?>
+
+                            <h4 class="price__subtitle">
+                                <?php e('subtitle'); ?>
+                            </h4>
+                            <?php if (have_rows('list')): ?>
+                                <ul class="price-list">
+                                    <?php while (have_rows('list')) : the_row(); ?>
+                                        <li class="price-list__item">
+                                            <div class="price-list__icon">
+                                                <img src="<?php e('icon'); ?>" alt="">
+                                            </div>
+                                            <div class="price-list__text">
+                                                <?php e('text'); ?>
+                                            </div>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
+                            <a href="#get-forecast" class="button--gradient price__button fancybox"><?php e('button_text'); ?></a>
+                        </div>
+                    </div>
+                </section>
+
+            <?php endif; ?>
+        <?php elseif ( get_row_layout() == 'screen_10' ): ?>
+            <?php if (!get_sub_field('screen_off')) : ?>
+
+                <section class="section reviews-section" id="<?php e('screen_id'); ?>" style="background-image: url(<?php e('screen_bg'); ?>);">
+                    <div class="container">
+                        <div class="reviews">
+                            <h3 class="section__title reviews__title">
+                                <?php e('title'); ?>
+                            </h3>
+                            <div class="reviews-container">
+
+                                <?php if (have_rows('list')): ?>
+
+                                    <div class="reviews-slider">
+                                        <?php while (have_rows('list')) : the_row(); ?>
+                                            <div>
+                                                <div class="reviews-slider__item">
+                                                    <a href="<?php e('photo'); ?>" class="reviews-slider__img fancybox">
+                                                        <img src="<?php e('photo'); ?>" alt="">
+                                                    </a>
+                                                    <div class="reviews-slider-container">
+                                                        <div class="reviews-slider__text">
+                                                            <?php e('text'); ?>
+                                                        </div>
+                                                        <div class="reviews-slider__name">
+                                                            <strong>
+                                                                <?php e('name'); ?>
+                                                            </strong>
+                                                            <?php e('position'); ?>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <?php endwhile; ?>
+                                    </div>
+
+                                <?php endif; ?>
+                                <?php if (have_rows('links')): ?>
+                                    <div class="reviews-list">
+                                        <?php while (have_rows('links')) : the_row(); ?>
+                                            <a target="_blank" href="<?php e('link'); ?>" class="reviews-list__item">
+                                                <img src="<?php e('icon'); ?>" alt="">
+                                            </a>
+                                        <?php endwhile; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            <?php endif; ?>
 
         <?php endif; ?>
 
